c1a34f513ea6ded9cc8edd5b9f75748c9ba4bc9c
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-01-07 12:07:05 +0100

    Progress skills bar script added

--- js/main.js
+++ js/main.js
@@ -2,16 +2,40 @@ const navBar = document.querySelector('.navbar')
 const navBarTopReset = navBar.offsetTop
 const sections = document.querySelectorAll('section')
 const navBarLinks = document.querySelectorAll('.navbar-link')
+const progress = document.querySelector('.skills-bars-wrapper')
+const progresBarValues = [85, 65, 70, 50, 70]
 
 window.addEventListener('scroll', () => {
-   window.pageYOffset >= navBarTopReset ? navBar.classList.add('sticky') : navBar.classList.remove('sticky')
+    pageScript()
 
-   sections.forEach((section, index) => {
-    if (window.pageYOffset >= section.offsetTop - 50) {
-      navBarLinks.forEach((navbarLink) => {
-        navbarLink.classList.remove('change');
-      });
-      navBarLinks[index].classList.add('change');
-    }
-  });
 })
+const pageScript = () =>{
+    window.pageYOffset >= navBarTopReset ? navBar.classList.add('sticky') : navBar.classList.remove('sticky')
+
+    sections.forEach((section, index) => {
+     if (window.pageYOffset >= section.offsetTop - 50) {
+       navBarLinks.forEach((navbarLink) => {
+         navbarLink.classList.remove('change');
+       });
+       navBarLinks[index].classList.add('change');
+     }
+   })
+   
+   if (window.pageYOffset + window.innerHeight >= progress.offsetTop - 50) {
+     document.querySelectorAll('.skill-progres').forEach((elemet, index) => {
+         elemet.style.width = `${progresBarValues[index]}%`
+         elemet.previousElementSibling.firstElementChild.textContent =
+         progresBarValues[index]
+         
+     })
+   }
+   else{
+     console.log(progress.offsetTop)
+    } 
+    
+}
+    pageScript()
+
+window.addEventListener("resize", () => {
+    window.location.reload();
+});
